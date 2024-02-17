caf186a99543d417d1b1b8f60325ddb79cc967c4
horacio barrios <horacioxbarrios@gmail.com>
2024-02-17 17:49:27 -0300

    Ejemplo de Propiedades con accesor Getter y Setter (usado en la tecnica de Encapsulacion) - control sobre los valores en los atributtos privados de una Clase.

---
+++ 3-1_POO Clases y Objetos/C_AppClasesYObjetos/C_AppClasesYObjetos/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace C_AppClasesYObjetos
+{
+    internal class Program // Class punto de inicio del programa
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Instanciamos el objeto Venta
+            Venta venta1 = new Venta(100, DateTime.Now); // le pasamos el total y el tiempo en que se produjo la venta
+            venta1.Show();
+            Console.WriteLine(venta1.GetInfo());
+
+
+
+            //Otra instancia , otro objeto Venta
+            Venta venta2 = new Venta(10000, DateTime.Now);
+            venta1.Show();
+            Console.WriteLine(venta1.GetInfo());
+        }
+    }// fin de class program
+
+
+
+
+
+
+
+
+    // Ejemplo de una Class
+    // Clase que representa una venta
+    class Venta
+    {
+        // Campos o atributos
+        int total;
+        DateTime date;
+
+
+        //Constructor que no recibe parametros
+        public Venta() { }
+
+        // Constructor que inicializa una venta con valores dados
+        public Venta(int total, DateTime date)
+        {
+            this.total = total;
+            this.date = date;
+        }
+
+
+        //metodos
+
+        // Método para mostrar un mensaje sobre la venta
+        public void Show()
+        {
+            Console.WriteLine("Hola soy una venta");
+        }
+
+        // Método para obtener información sobre la venta
+        public string GetInfo()
+        {
+            return "Total a Pagar: " + total + " " + "Fecha: " + date.ToLongDateString(); //el metodo de la clase Date se puede usar en un objeto del mismo tipo. aca el metodo ToLongDateString() combierte el dato en string
+
+        }
+    }
+}
---
+++ 3-2_POO Propiedades Get- Set/CAppPropiedades/CAppPropiedades/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace CAppPropiedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Venta venta1 = new Venta(100, DateTime.Now);
+            venta1.Total = -153652555;
+            Console.WriteLine(venta1.Total); // usamos la propiedad Total y nos hace de Get para traernos el valor guardado en _total 
+
+            //venta1.Date = DateTime.Now; // No me permite settear ya que mi propiedad no tiene set
+            Console.WriteLine(venta1.Date); // Solo tenemso el Accesor Get
+        }
+    }
+
+
+
+
+    class Venta
+    {
+        // Los atributos sin modificador de acceso se establecen como private de forma implícita.
+        private int _total; // Por convención, se puede usar guion bajo para nombrar atributos privados.
+        DateTime date;
+
+
+
+        /* Encapsulación:
+               Para tener un control de los datos se utiliza la encapsulación. Es decir, los atributos que no 
+               queremos que sean accedidos desde cualquier lado o modificados de cualquier forma se definen como privados.
+
+               Para poder tener acceso controlado a dicho atributo se utilizan las propiedades Get y Set. Estas propiedades deben tener acceso public.
+        */
+
+
+        public Venta(int total, DateTime date)
+        {
+            _total = total;// En este caso, se asigna el valor del parámetro 'total' al atributo '_total'. No es necesario usar 'this' ya que los nombres son distintos.
+            this.date = date;// 'this' se utiliza para diferenciar entre el parámetro 'date' y el atributo 'date' de la clase.
+        }
+
+
+
+
+
+
+        // Tenemos acceso al atributo privado _total con la propiedad Total que es public y tenemos control sobre dicho atributo.
+
+        //public int Total 
+        //{
+        //    get { return _total.ToString(); } // Nos da error porque estamos definiendo el retorno en Int en la declaracion de la propiedad Total y estamos convirtiendo el valor que se retorna a string
+        //    set { _total = value;}
+        //}
+
+
+
+
+        // otro ejemplo
+        //public string Total
+        //{
+        //    get
+        //    {
+        //        return _total.ToString();
+        //    }
+
+        //}
+
+
+
+        // Propiedad para obtener y establecer el total de la venta.
+
+        // Propiedad con getter y setter
+        public int Total
+        {
+            get // Accesor de lectura (getter)
+            { 
+                return _total; 
+            }
+            set // Accesor de escritura (setter)
+            { 
+                if(value < 0) // Se verifica que el valor no sea negativo antes de asignarlo.
+                {
+                    value = 0;
+                }
+                _total = value;
+            }
+
+        }
+
+
+        public string Date
+        {
+            get 
+            {
+                return this.date.ToString();
+            }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("Hola soy una venta");
+        }
+
+
+        public string GetInfo()
+        {
+            return "Total a Pagar: " + _total + " " + "Fecha: " + date.ToLongDateString(); 
+
+        }
+    }
+}
