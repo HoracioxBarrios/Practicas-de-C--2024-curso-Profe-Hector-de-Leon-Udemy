30e72d94a4eff1a3f18648073259e5c065502ce5
horacio barrios <horacioxbarrios@gmail.com>
2024-02-19 13:28:07 -0300

    Sobreescritura de Metodo -  Polimorfismo en tiempo de ejecucion

--- 3-5_POO Sobreescritura de Metodos/ConsoleAppSobreescrituraMet/ConsoleAppSobreescrituraMet/Program.cs
+++ 3-5_POO Sobreescritura de Metodos/ConsoleAppSobreescrituraMet/ConsoleAppSobreescrituraMet/Program.cs
@@ -9,6 +9,18 @@
 
             C objetoC = new C();
             Console.WriteLine(objetoC.Saludar());
+
+
+            //Ejemplo 2
+            Sale sale = new Sale(10);// cuantos elementos vamos a manejar
+            sale.Add(2);// una cantidad un producto, un precio
+            sale.Add(3);
+            Console.WriteLine("El total es: " + sale.GetTotal());// imprimimos el total
+
+            SaleWhithTax saleWithTax = new SaleWhithTax(10, 1.16m); //al inicializar variables decimales, es necesario agregar el sufijo m o M al final del número para indicar que se trata de un decimal. 
+            saleWithTax.Add(2);
+            saleWithTax.Add(3);
+            Console.WriteLine("El total con impuesto es: " + saleWithTax.GetTotal());
         }
     }
 
@@ -54,6 +66,69 @@
             return base.Saludar() + " y de C";//base hace referencia al Padre
         }
     }
+
+
+
+
+
+
+
+    //Ejemplo 2 'mas real'
+    public class Sale
+    {
+        private decimal[] _amounts;
+        private int _n;
+        private int _end;
+
+
+        public Sale(int n) 
+        {
+            _amounts = new decimal[n];
+            _n = n;
+            _end = 0;        
+        }
+
+        public void Add(decimal amount)
+        {
+            if (_end < _n)
+            {
+                _amounts[_end] = amount;
+                _end++;
+            }
+        }
+        public virtual decimal GetTotal()
+        {
+            decimal result = 0;
+            int i = 0;
+
+            while(i < _amounts.Length)
+            {
+                result += _amounts[i];
+                i++;
+            }
+
+            return result;
+        }
+    }
+
+    //Supongamos que necesitamos obtener el total de una venta pero con un impuesto agregado.
+    //Podemos crear una class que herede de Sale con un metodo al que vamos a sobreeescribir para ampliar su funcionalidad.
+    public class SaleWhithTax : Sale
+    {
+        private decimal _tax;
+        public SaleWhithTax(int n, decimal tax) : base(n)// Tenemos que pasarle el parametro al Padre
+        {
+            _tax = tax;
+        }
+
+        public override decimal GetTotal()
+        {
+            return base.GetTotal() * _tax; // ampliamos la funcionalidad agregandole el impuesto
+        }
+
+    }
+
+
 }
 
 /*
