f1a3ccf25bc1310e0545329c0a5c3d4edd207621
horacio barrios <horacioxbarrios@gmail.com>
2024-02-27 23:41:49 -0300

    Intro a Sql

---
+++ 8-0_Base de Datos- Intro Sql/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,101 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+/*  Que es SQL?
+    ---------------------
+    SQL, significa Structured Query Language (Lenguaje de Consulta Estructurado),
+    es un lenguaje de programación diseñado para administrar y manipular bases de datos relacionales.
+    Fue desarrollado originalmente por IBM en la década de 1970 y desde entonces ha sido adoptado como
+    el estándar de facto para la gestión de bases de datos.
+
+    SQL permite a los usuarios realizar una variedad de operaciones en una base de datos, incluyendo:
+
+    1. Consultas: Recuperar datos específicos de una base de datos utilizando la cláusula SELECT.
+
+    2. Inserciones: Agregar nuevos datos a una tabla utilizando la cláusula INSERT.
+
+    3. Actualizaciones: Modificar datos existentes en una tabla utilizando la cláusula UPDATE.
+
+    4. Eliminaciones: Eliminar datos de una tabla utilizando la cláusula DELETE.
+
+    5. Definición de esquemas: Crear, modificar y eliminar tablas y otros objetos de base de datos
+       utilizando las cláusulas CREATE, ALTER y DROP.
+
+    6. Control de acceso: Establecer permisos y roles de usuario para controlar quién puede acceder
+       y modificar los datos en la base de datos.
+
+
+
+
+
+    SQL es un lenguaje poderoso y versátil que se utiliza en una amplia gama de aplicaciones y entornos,
+    desde sistemas de gestión de bases de datos empresariales hasta aplicaciones web y móviles.
+    Es fundamental para interactuar con datos almacenados de manera estructurada y realizar operaciones
+    complejas de consulta y manipulación.
+*/
+
+
+
+/*  que es SQL Server ?
+    --------------------
+    SQL Server es un sistema de gestión de bases de datos relacionales (RDBMS) desarrollado por Microsoft. 
+    Es un software que permite almacenar y recuperar datos solicitados por otras aplicaciones de software, 
+    ya sea en la misma computadora donde está instalado SQL Server o en otra computadora a través de una red.
+
+    SQL Server utiliza un lenguaje de consulta llamado SQL (Structured Query Language) para interactuar 
+    con las bases de datos. SQL Server admite una variedad de funciones, incluidas transacciones, replicación 
+    de datos, almacenamiento de procedimientos almacenados y consultas complejas.
+
+    Entre sus características más destacadas se incluyen:
+
+    1. Seguridad: SQL Server ofrece funciones de seguridad robustas que permiten controlar el acceso a 
+    los datos y proteger la integridad de la información.
+
+    2. Escalabilidad: Puede manejar grandes volúmenes de datos y escalar vertical y horizontalmente para 
+    satisfacer las demandas de aplicaciones empresariales.
+
+    3. Disponibilidad: SQL Server ofrece opciones de alta disponibilidad, como la replicación, la 
+    agrupación de conmutación por error y la copia de seguridad y restauración, para garantizar que los 
+    datos estén disponibles cuando se necesiten.
+
+    4. Integración: Está estrechamente integrado con otras tecnologías de Microsoft, como .NET Framework, 
+    Visual Studio y Azure, lo que facilita el desarrollo de aplicaciones y la administración de bases de datos 
+    en entornos Windows.
+
+    En resumen, SQL Server es una plataforma de base de datos potente y ampliamente utilizada en entornos 
+    empresariales para almacenar, administrar y recuperar datos de manera eficiente y segura.
+*/
+
+
+/*  Que es SSMS ?
+    -------------------
+    SQL Server Management Studio (SSMS) es una interfaz gráfica de usuario desarrollada por Microsoft que se 
+    utiliza para administrar y trabajar con instancias de SQL Server. SSMS proporciona una amplia gama de 
+    herramientas y funciones que permiten a los desarrolladores y administradores de bases de datos interactuar 
+    con las bases de datos SQL Server de manera eficiente y conveniente.
+
+    Algunas de las características principales de SQL Server Management Studio incluyen la capacidad de 
+    crear y administrar bases de datos, escribir y ejecutar consultas SQL, diseñar y modificar esquemas de 
+    base de datos, configurar seguridad y permisos, monitorear el rendimiento de las bases de datos, y realizar 
+    tareas de mantenimiento y administración del servidor.
+
+
+
+    SSMS es una herramienta esencial para cualquier persona que trabaje con bases de datos SQL Server, 
+    ya sea en entornos de desarrollo, pruebas o producción. Proporciona una interfaz intuitiva y poderosa 
+    que facilita la administración y el mantenimiento de bases de datos SQL Server, lo que ayuda a mejorar 
+    la productividad y eficiencia de los desarrolladores y administradores de bases de datos.
+*/
+
+
+//INSTALAR: SQL SERVER 2022 - Motor de la base de datos : https://www.microsoft.com/es-ar/sql-server/sql-server-downloads (instalacion basica - en ingles) Instrucciones: https://www.youtube.com/watch?v=KhteFomFPNo&t=3s
+
+//Descarga INSTALA SQL Server Management Studio (SSMS): https://learn.microsoft.com/es-es/sql/ssms/download-sql-server-management-studio-ssms?view=sql-server-ver16#whats-new al 27-02-2024 la version es la 19.0.3
