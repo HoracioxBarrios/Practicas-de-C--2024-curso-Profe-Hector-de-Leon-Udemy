8914059b4419fc9af1fbc5fae6dd22d52b953a5d
horacio barrios <horacioxbarrios@gmail.com>
2024-02-29 04:58:50 -0300

    Base de Datos: Conexion

---
+++ 8-3_Base de Datos - Conexion/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using Libreria;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                DB baseDato = new DB(@"HORA\SERVER_PRUEBA", "CsharpDB", "sa", "123456");
+                baseDato.Connect();
+
+                // ACA DEBERIA IR LA CONSULTA
+
+                baseDato.Close();
+
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine("Error al conectar con la DB");
+            }
+        }
+    }
+}
---
+++ 8-3_Base de Datos - Conexion/ConsoleApp/Libreria/DB.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+
+namespace Libreria
+{
+    public class DB // despues la vamos a hacer abtracta
+    {
+        private string _connectionString;
+        protected SqlConnection _connection; // SqlConnection (Acciones rapidas e instalar libreria SqlClient)
+                                            //Solo queda definidio: _connection
+
+
+
+        public DB(string server, string baseDatos, string user, string password)
+        {
+            _connectionString = $"Data Source={server}; Initial Catalog={baseDatos};" + $"User={user}; Password={password}"; 
+        }
+
+
+
+
+
+        //MÉTODO QUE VA A HACER LA CONEXION
+        public void Connect()
+        {
+            _connection = new SqlConnection(_connectionString); //Instancia la conecion
+            _connection.Open();
+            Console.WriteLine("Se abrio la conexion");
+        }
+
+
+        // MÉTODO PARA CERRAR LA CONEXION 
+        public void Close()
+        {
+            if (_connection != null && _connection.State == System.Data.ConnectionState.Open) // protejemos el close() si _conection no es null y si el estado es open, podemos cerrar porque está abierto.
+            {
+                _connection.Close();
+                Console.WriteLine("Se cerro la conexion");
+            }
+        }
+
+    }
+}
