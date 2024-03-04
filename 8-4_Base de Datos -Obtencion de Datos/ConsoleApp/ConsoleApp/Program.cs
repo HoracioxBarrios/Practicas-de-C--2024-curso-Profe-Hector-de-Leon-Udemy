e163ab79dab071975a279fa1ae4b397ca9deaabf
horacio barrios <horacioxbarrios@gmail.com>
2024-03-04 05:32:27 -0300

    Obtenemos los datos de la db, manejamos esos datos en BeerDB y con el metodo GetAll devolvemos una lista de cervezas.

---
+++ 8-4_Base de Datos -Obtencion de Datos/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using Libreria;
+using System.Data.SqlClient;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                BeerDB beerDB = new BeerDB(@"HORA\SERVER_PRUEBA", "CsharpDB", "sa", "123456");
+                // ACA DEBERIA IR LA CONSULTA
+                List<Beer> listBeers = beerDB.GetAll(); //GetAll consulta la db, se trae los datos y devuelve una lista de Cervezas
+
+                foreach (Beer beer in listBeers)
+                {
+                    Console.WriteLine(beer);
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine("Error al conectar con la DB");
+            }
+
+
+        }
+    }
+}
+
+
+/*
+ Es importante tener el MOTOR DE LA BASE DE DATOS (Sql server) Inicializado.
+supongamos que lo desactivamos en el administrador de tareas de windows,
+podemos buscar con la directiva (Servicios) SQL SERVER ej: MISERVER
+e iniciar. con el servidor iniciado ya podemos conectarnos con db.Connect()
+ */
---
+++ 8-4_Base de Datos -Obtencion de Datos/ConsoleApp/Libreria/BeerDB.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
+{
+    public class BeerDB : DB
+    {
+        
+        public BeerDB(string server, string db, string user, string pass) : 
+            base(server, db, user, pass)
+        {
+            /*
+             Los valores de server, db, user, pass se manejan en la clase base.
+             El constructor del padre hace la tarea del conection string.
+             Nosotros necesitamos el obj que tiene la conexion.
+             */
+        }
+        //Método que nos regrese una lista de Beers en base a los datos de la Base de datos.
+
+        public List<Beer> GetAll()
+        {
+            base.Connect();
+            List<Beer> listBeer = new List<Beer>();
+            string query = "SELECT Id, Name, BrandId FROM BEER"; //consulta
+            SqlCommand command = new SqlCommand(query, _connection); //SqlCommand va a mandar comandos a la db, en este caso la consulta (query) ---- SqlComand espera la consulta, y la conexion (que la tenemos creada en la clase Padre DB como protected _connection) la clase derivada puede hacer uso de los atribut protected, pero no de lo private que es de propio de la Clase
+            //Necesitamos ahora ejecutar ese comando
+            SqlDataReader reader = command.ExecuteReader(); //ExecuteReader() devuelve un tipo SqlDataReader que es en si la informacion conseguida de la base de datos, pero necesitamos tratar los datos del siguiente modo, para que este metodo devuelve la lista de cervezas.
+
+            //vamos a leer el reader siempre que haya algo por leer, o siempre que exista una fila.
+            while(reader.Read())
+            {
+                int id = reader.GetInt32(0);// primer elemento es la id
+                string name = reader.GetString(1);//segundo elemento es el name
+                int brandId = reader.GetInt32(2);
+                listBeer.Add(new Beer(id, name, brandId));// creamos la Cerveza y la Guardamos a la Lista
+            }
+
+
+            base.Close();
+            return listBeer;
+        }
+
+
+
+    }
+}
