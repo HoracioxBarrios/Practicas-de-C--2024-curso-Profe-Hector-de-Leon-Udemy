1c9d79f731da41aead7f2caed50350b97456fde6
horacio barrios <horacioxbarrios@gmail.com>
2024-03-05 06:57:24 -0300

    Entity Framework Dependencias instalacion y mapeo de Entidades de la DB.

---
+++ 9-0_ Entity Framework - Intro e Instalacion/EntityDB/ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+/*Paso a Paso para Instalar Entity Framework 
+    1_ Tener la Base de datos con las tablas y relacionadas con primary key - foraight key (al menos algo)
+    2_ crear un proyecto de libreria de clases para trabajar, e instalar en ese proyecto las siguiente dependencias:
+    3_ sobre dependencias click derecho /administrar paquetes nugets:
+    INSTALAR:
+    Microsoft.EntityFrameworkCore
+    Microsoft.EntityFrameworkCore.SqlServer
+    Microsoft.EntityFrameworkCore.Tools
+    
+    Posicionarse sobre el proyecto de Libreria y seleccionarla, luego ir arriba a -> Herramientas / Adminstrador paquetes NuGets/ Consola de adm de Paquetes
+    
+
+
+ EXPLICACION:   contexto      /    server                 / Db               / User / Pass          / Autenticacion para windows (sino da error ssl de permisos) / el proveeedor sql.
+COMANDO PARA LA CONSOLA NUGETS:
+PM> Scaffold-DbContext "Server=Hora\SERVER_PRUEBA;Database=CsharpDB; User=sa; Password=123456;TrustServerCertificate=Yes;" Microsoft.EntityFrameworkCore.SqlServer
+
+Esto nos mapea las entidades de la DB en clases C# para poder trabajar.
+
+
+
+
+Si se modifica la db con nuevas tablas y queremos remapear podemos forzar el remapeo:
+PM> Scaffold-DbContext "Server=Hora\SERVER_PRUEBA;Database=CsharpDB; User=sa; Password=123456;TrustServerCertificate=Yes;" Microsoft.EntityFrameworkCore.SqlServer -force
+(No debe de tener errores el codigo c#, sino no tomará eñ comando)
+ */
