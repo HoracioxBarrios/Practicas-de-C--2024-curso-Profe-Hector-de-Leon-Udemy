c794e8eb4643d1348fb41d09940e9d813491f13b
horacio barrios <horacioxbarrios@gmail.com>
2024-03-13 08:45:01 -0300

    Delegado Generico Func - Puede no recibir argumentos, o recibir entre 1 y 16 ; Pero Debe si o si retornar algo.

---
+++ 10-4_Programacion Funcional -Delegado Generico Func/DelegadoApp/DelegadoApp/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+    En C#, un delegado genérico Func es una clase predefinida que 
+    encapsula un método que devuelve un valor y puede aceptar de 1 
+    hasta 16 parámetros de entrada genéricos. La sintaxis básica 
+    de un delegado Func es la siguiente:
+    
+    FIRMA DEL DELEGADO
+                // Declarar un delegado Func<int, bool> que representa un método que toma un entero y devuelve un booleano.
+    Func<int, bool> esParDelegado = EsPar;
+     
+                // Llamar al delegado con un entero y mostrar el resultado.
+    int numero = 6;
+    bool resultado = esParDelegado(numero);
+    
+    Console.WriteLine($"¿Es {numero} un número par?: {resultado}"); //True
+
+
+
+
+               // Método que corresponde a la firma requerida por el delegado Func<int, bool>.
+    static bool EsPar(int num)
+    {
+        return num % 2 == 0;
+    }
+ */
+
+#region Func
+//EJEMPLO QUE NO RECIBE ARGUMENTOS (si o si debe retornar algo)
+//Func debe regresar algo (debe tener un tipo de retorno)
+Func<int> numberRamdom = () => new Random().Next(0, 100); //retorn int
+
+
+Console.WriteLine(numberRamdom());
+
+
+//EJEMPLO QUE RECIBE ARGUMENTOS (si o si debe retornar algo)
+int numeroLimit = 1000;
+// Func<Recibe, Retorna>
+/*Func<int, int> numberRamdonLimit = (limit) => new Random().Next(0, limit);*/ //en una linea no usar return
+
+Func<int, int> numberRamdonLimit = (limit) =>
+{
+    return new Random().Next(0, limit);
+};//en multilinea con {} hay que usar la palabra return
+
+Console.WriteLine(numberRamdonLimit(numeroLimit));
+#endregion
